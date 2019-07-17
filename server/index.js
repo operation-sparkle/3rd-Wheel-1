c9b4b159793b56bc94882bfdc303e833408ca9d7
AS Wilson <alexander.wilson@ncf.edu>
2019-07-17 18:33:56 -0500

    Require models into server index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const { User, Date, UserInterest, Couple, Category, Spot } = require('../database/models/index.js');
 
 const app = express();
 
