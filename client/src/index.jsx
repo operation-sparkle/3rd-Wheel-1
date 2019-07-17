57cb2b007c1256ae6309c6a14c4d24feb343330c
Charles Teamer <cmo1462@gmail.com>
2019-07-17 16:29:06 -0500

    half-nav

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Bootstrap from 'bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class App extends React.Component {
   constructor (props) {
@@ -12,7 +15,13 @@ class App extends React.Component {
   render() {
     return (
       <div className="App" >
-        <h1>3rd Wheel</h1>
+        {/* <h1>3rd Wheel</h1> */}
+        <Navbar fixed="top" expand="true" bg="dark" variant="dark">
+          {/* Link brand to hot dates */}
+          <Navbar.Brand>3rd Wheel</Navbar.Brand>
+          <Navbar.toggle> </Navbar.toggle>
+          <Nav.link> Profile </Nav.link>
+        </Navbar>
       </div>
     );
   }
