b37892c047c19582c1a362a500508a6c7ecbbce0
Operation Spark <miguelpeter2@gmail.com>
2019-07-17 14:59:24 -0500

    Merging with all other files

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = (grunt) => {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: 'client/dist/bundle.js',
+        dest: 'dist/built.js',
+      },
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'dist/built.js': ['dist/built.js'],
+        }
+      }
+    },
+
+    eslint: {
+      options: {
+        quiet: true,
+      },
+      target: {
+        ['dist/built.js'],
+      },
+    },
+
+    cssmin: {
+      'dist/output.css': ['client/src/App.css'],
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('build', [
+    'concat', 'uglify', 'eslint', 'cssmin',
+  ]);
+};
