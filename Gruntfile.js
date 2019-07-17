aeedc8000212bb1f9eebb853474dd1a37a58ef60
AS Wilson <alexander.wilson@ncf.edu>
2019-07-17 10:13:43 -0500

    Load eslint in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,12 @@ module.exports = (grunt) => {
     pkg: grunt.file.readJSON('package.json'),
   });
 
+
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-eslint');
 
   grunt.registerTask();
 };
