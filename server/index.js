9a8d71e2db4e59e1182beedea28bcd44cb5083dd
AS Wilson <alexander.wilson@ncf.edu>
2019-07-17 15:29:36 -0500

    Require and use body-parser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
