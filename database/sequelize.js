3ef4b7a95ad280384501217e15d355cdc44a6421
TheFranzy <galen.housego@gmail.com>
2019-07-17 11:03:48 -0500

    placed foreign keys associated to each table and the hotspot

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,44 +1,46 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('3rd-wheel', 'root', '', {
+const sequelize = new Sequelize('3rd-wheel', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 })
 
-const users = db.define('users', {
+const users = sequelize.define('users', {
     id: Sequelize.NUMBER,
     name: Sequelize.STRING,
-    interests: Sequelize.STRING
-});
+    interests: Sequelize.STRING,
+    bio: Sequelize.STRING,
+    comments: Sequelize.STRING
+}, {sequelize, modelName: 'users' });
 
-const restaurants = db.define('restaurants', {
-    id: Sequelize.NUMBER,
-    name: Sequelize.STRING,
-    type: Sequelize.STRING,
-    location: Sequelize.STRING
-});
-
-const entertainment = db.define('entertainment', {
+const restaurants = sequelize.define('restaurants', {
     id: Sequelize.NUMBER,
     name: Sequelize.STRING,
     type: Sequelize.STRING,
     location: Sequelize.STRING
-});
+}, {sequelize, modelName: 'restaurants' });
 
-const activity = db.define('activity', {
+const entertainment = sequelize.define('entertainment', {
     id: Sequelize.NUMBER,
     name: Sequelize.STRING,
     type: Sequelize.STRING,
     location: Sequelize.STRING
-});
+}, {sequelize, modelName: 'entertainment' });
 
-const hotSpots = db.define('hotspot', {
+const activity = sequelize.define('activity', {
     id: Sequelize.NUMBER,
     name: Sequelize.STRING,
-    rating: Sequelize.NUMBER,
-});
+    type: Sequelize.STRING,
+    location: Sequelize.STRING
+}, {sequelize, modelName: 'activity' });
 
-exports.db = db;
+const hotSpots = sequelize.define('hotspot', {
+    id: Sequelize.NUMBER,
+}, {sequelize, modelName: 'hotspot' });
+
+hotSpots.belongsTo(users, restaurants, entertainment, activity);
+
+exports.sequelize = sequelize;
 exports.users = users;
 exports.restaurants = restaurants;
 exports.entertainment = entertainment;
