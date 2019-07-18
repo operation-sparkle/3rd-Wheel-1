ed66a28ce6203e3991a39e39cd40bfaab40d1366
Operation Spark <miguelpeter2@gmail.com>
2019-07-18 09:25:32 -0500

    Imported components react-bootstrap and appended an image to page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
+const Profile = (props) => {
 
-const Profile = () => {
   return (
+
+    <Container>
+  <Row>
+    <Col xs={6} md={4}>
+      <Image src="https://cdn.pixabay.com/photo/2015/08/14/19/42/frog-888798_960_720.jpg" thumbnail />
+    </Col>
+  </Row>
+</Container>
+
 
   );
 }
