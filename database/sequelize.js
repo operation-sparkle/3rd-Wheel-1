c63c6d34f3f4ea64682ff8d6e12192f47db25e7c
TheFranzy <galen.housego@gmail.com>
2019-07-18 11:33:09 -0500

    database online

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,88 +1,95 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const Sequelize = require('sequelize');
 const bcrypt = require('bcrypt');
+const { Model } = Sequelize;
 
 const sequelize = new Sequelize('3rd_wheel', 'root', '', {
-  host: 'localhost',
+//   host: 'localhost',
   dialect: 'mysql',
 });
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.NUMBER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+// const User = sequelize.define('user', {
+class User extends Model {}
+User.init({
+//   id: {
+//     type: Sequelize.NUMBER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
   name: Sequelize.STRING,
   pic: Sequelize.STRING,
   password: Sequelize.STRING,
   salt: Sequelize.STRING,
-  age: Sequelize.NUMBER,
+  age: Sequelize.INTEGER,
   interests: Sequelize.STRING,
   bio: Sequelize.STRING,
-  latitude: Sequelize.NUMBER,
-  longitude: Sequelize.NUMBER,
+  latitude: Sequelize.INTEGER,
+  longitude: Sequelize.INTEGER,
+
 }, {
-  hooks: {
-    beforeCreate: (user) => {
-      const salt = bcrypt.genSaltSync(10);
-      user.password = bcrypt.hashSync(user.password, salt);
-    },
-  },
+  freezeTableName: true,
+  //   hooks: {
+  //     beforeCreate: (user) => {
+  //       const salt = bcrypt.genSaltSync(10);
+  //       user.password = bcrypt.hashSync(user.password, salt);
+  //     },
+  //   },
   instanceMethods: {
-    // generateHash(password) {
-    //   return bcrypt.genSalt(10)
-    //     .then(salt => bcrypt.hash(password, salt));
-    // },
+    generateHash(password) {
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(password, salt));
+    },
     validPassword(password) {
       return bcrypt.compare(password, this.password);
     },
   },
-},
-{ sequelize, modelName: 'user' });
+  sequelize,
+  modelName: 'user',
+});
+// { sequelize, modelName: 'user' });
 
 const Date = sequelize.define('date', {
-  id: {
-    type: Sequelize.NUMBER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+//   id: {
+//     type: Sequelize.NUMBER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
 }, { sequelize, modelName: 'date' });
 
 const UserInterest = sequelize.define('userInterest', {
-  id: {
-    type: Sequelize.NUMBER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+//   id: {
+//     type: Sequelize.NUMBER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
 }, { sequelize, modelName: 'userInterest' });
 
 const Couple = sequelize.define('couple', {
-  id: {
-    type: Sequelize.NUMBER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+//   id: {
+//     type: Sequelize.NUMBER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
 }, { sequelize, modelName: 'couple' });
 
 const Category = sequelize.define('category', {
-  id: {
-    type: Sequelize.NUMBER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+//   id: {
+//     type: Sequelize.NUMBER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
   name: Sequelize.STRING,
 }, { sequelize, modelName: 'category' });
 
 const Spot = sequelize.define('spot', {
-  id: {
-    type: Sequelize.NUMBER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+//   id: {
+//     type: Sequelize.NUMBER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
   name: Sequelize.STRING,
-  latitude: Sequelize.NUMBER,
-  longitude: Sequelize.NUMBER,
+  latitude: Sequelize.INTEGER,
+  longitude: Sequelize.INTEGER,
 }, { sequelize, modelName: 'spot' });
 
 Category.belongsTo(Category, { as: 'children', foreignKey: 'parentId', useJunctionTable: false });
@@ -93,7 +100,7 @@ UserInterest.belongsTo(User);
 Couple.belongsTo(User, { as: 'user1Id' });
 Couple.belongsTo(User, { as: 'user2Id' });
 
-sequelize.sync()
+sequelize.sync({ force: true })
   .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
   .catch(error => console.log('This error occured', error));
 
