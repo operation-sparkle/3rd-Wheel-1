a0a06416e260f050b478ba0ea4ae2fc577a53cde
Operation Spark <miguelpeter2@gmail.com>
2019-07-18 09:31:39 -0500

    Imported components from react-boostrap and all subcomponents from components file. Added function that renders components to page when tab is clicked in NavBar

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,23 @@ import ReactDOM from 'react-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
+
+import Profile from './components/Profile';
+import Interests from './components/Interests';
+import Pending from './components/Pending';
+import Matches from './components/Matches';
+
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      username: "",
+    }
+    this.transition = this.transition.bind(this);
+  }
+  transition(linked) {
+    return function() {
+      ReactDOM.render(linked(), document.getElementById("body"));
     }
   }
 
@@ -20,15 +32,15 @@ class App extends React.Component {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="./components/Profile">Profile</Nav.Link>
-            <Nav.Link href="./components/Interests">Interests</Nav.Link>
-            <Nav.Link href="./components/Pending">Pending</Nav.Link>
-            <Nav.Link href="./components/Matches">Matches</Nav.Link>
+            <Nav.Link onClick={this.transition(Profile)}>Profile</Nav.Link>
+            <Nav.Link onClick={this.transition(Interests)}>Interests</Nav.Link>
+            <Nav.Link onClick={this.transition(Pending)}>Pending</Nav.Link>
+            <Nav.Link onClick={this.transition(Matches)}>Matches</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     </div>
-    );
+    )
   }
 }
 
