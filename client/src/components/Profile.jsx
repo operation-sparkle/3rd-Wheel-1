c9f402ab0244611a46bca1a7edad22c3e66176dc
Operation Spark <miguelpeter2@gmail.com>
2019-07-19 13:48:55 -0500

    Switched divs with cards as a test

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -5,19 +5,36 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
 
 const Profile = (props) => {
 
   return (
+    <div>
+    <Card style={{ width: '24rem' }}>
+      <Card.Img variant="top" src="https://im.indiatimes.in/content/itimes/photo/2016/Oct/5/1475667154-funniest-baby-jokes-ever.jpg" />
+      <Card.Body>
+        <Card.Title>My Name</Card.Title>
+      </Card.Body>
+        <ListGroup className="list-group-first">
+          <ListGroupItem>My Age</ListGroupItem>
+          <ListGroupItem>My Gender</ListGroupItem>
+          <ListGroupItem>My Preference</ListGroupItem>
+        </ListGroup>
+      <Card.Body>
+        <Card.Text>
+          Here's a small summary of myself.
+        </Card.Text>
+      </Card.Body>
+      </Card>
 
-    <Container>
-  <Row>
-    <Col xs={6} md={4}>
-      <Image src="https://cdn.pixabay.com/photo/2015/08/14/19/42/frog-888798_960_720.jpg" thumbnail />
-    </Col>
-  </Row>
-</Container>
+      <Container>
+        <span>Give me some text or something.</span>
+      </Container>
 
+      </div>
 
   );
 }
