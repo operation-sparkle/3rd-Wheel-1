f7fc04dcbf319acdf09c5f603841457f977db182
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 15:33:39 -0500

    Properly format api request

--- database/helpers/db-helpers.js
+++ database/helpers/db-helpers.js
@@ -2,16 +2,20 @@ const axios = require('axios');
 
 const restCategories = () => {
   const options = {
+    method: 'get',
     url: 'https://api.yelp.com/v3/categories',
-    Authorization: `Bearer ${process.env.YELP_KEY}`,
+    headers: { 'Authorization': `Bearer ${process.env.YELP_KEY}` },
   };
-  return axios.get(options.url)
+  return axios(options)
     .then((response) => {
-      return response.categories.filter(category => category.parent_aliases === 'restaurants')
-        .map(parentAlias => ({
-          name: parentAlias.text,
-          alias: parentAlias.alias,
-        }));
+      const { categories } = response.data;
+      const filteredCategories = categories.filter((category) => {
+        return category.parent_aliases.includes('restaurants');
+      });
+      return filteredCategories.map(parentAlias => ({
+        name: parentAlias.title,
+        alias: parentAlias.alias,
+      }));
     })
     .catch((err) => {
       console.error(`category error: ${err}`);
