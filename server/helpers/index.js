5d1eaa913421479f69cc99c747828a3fd2a86b02
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 13:52:49 -0500

    Remove restCategories helper

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -15,23 +15,6 @@ const getRestaurant = (location, categories) => {
     .catch(err => console.log(err, 'axios error'));
 };
 
-const restCategories = () => {
-  const options = {
-    url: 'https://api.yelp.com/v3/categories',
-    Authorization: `Bearer ${config.YELP_KEY}`,
-  };
-  axios.get(options.url)
-    .then((responce) => {
-      responce.categories.filter(category => category.parent_aliases === 'restaurants')
-        .map(parentAlias => ({
-          name: parentAlias.text,
-          alias: parentAlias.alias,
-        }));
-    })
-    .catch(err => console.log(`category error: ${err}`));
-};
-
 module.exports = {
   getRestaurant,
-  restCategories,
 };
