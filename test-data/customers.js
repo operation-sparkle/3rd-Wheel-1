13bc716ab99ba56ff7acb92e645f4933cd511057
Operation Spark <miguelpeter2@gmail.com>
2019-07-19 14:20:01 -0500

    Inserted test customer data

--- test-data/customers.js
+++ test-data/customers.js
@@ -1,3 +1,16 @@
-module.exports = [
-
-]
+module.exports = [{
+  name: 'Tiny',
+  gender: 'male',
+  age: 21,
+  img: 'https://i.imgur.com/T7vjtfH.jpg',
+}, {
+  name: 'Tiny',
+  gender: 'male',
+  age: 21,
+  img: 'https://i.imgur.com/9surkzy.jpg',
+}, {
+  name: 'Tiny',
+  gender: 'male',
+  age: 21,
+  img: 'https://i.imgur.com/gnuDKcN.jpg',
+}];
