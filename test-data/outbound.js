7909146ab18fc53148e32a986949ef4f8c8fa395
Operation Spark <miguelpeter2@gmail.com>
2019-07-19 17:42:07 -0500

    Added outbound data for testing

---
+++ test-data/outbound.js
@@ -0,0 +1,16 @@
+module.exports = [{
+  name: 'Dyno',
+  gender: 'male',
+  age: 10000,
+  img: 'https://www.elitesingles.com/sites/www.elitesingles.com/files/styles/elite_rectangle_article_arrow_left_frame/public/2b_en_ta_teaser_sp1.png',
+}, {
+  name: 'Enora',
+  gender: 'male',
+  age: 32,
+  img: 'https:twistedsifter.files.wordpress.com/2012/09/trippy-profile-pic-portrait-head-on-and-from-side-angle.jpg?w=800&h=700',
+}, {
+  name: 'Erin',
+  gender: 'male',
+  age: 20,
+  img: 'https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format&q=70',
+}];
