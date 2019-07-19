b51d102cfd46a95134026745ed347084f1110b9d
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 09:37:49 -0500

    Implement GET /categories

--- server/index.js
+++ server/index.js
@@ -71,6 +71,18 @@ app.get('/users/:id', loggedIn, (req, res) => {
     });
 });
 
+//  This retrieves the top-level categories ie Restaurants
+app.get('/categories', (req, res) => {
+  return Category.findAll({ where: { parentId: null } })
+    .then((categories) => {
+      res.status(200).send(categories);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
 app.get('/interests/:userId', loggedIn, (req, res) => {
   //  this is to find new spots around the user
 });
