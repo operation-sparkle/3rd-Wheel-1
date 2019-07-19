aca2ae50f3bd47bd4c2b7bd28fa97b23047aa7bd
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 14:21:56 -0500

    Implement populateCategories

--- database/models/index.js
+++ database/models/index.js
@@ -1,10 +1,29 @@
-
-
 const {
   User, Date, UserInterest, Couple, Category, Spot,
 } = require('../sequelize');
+const { restCategories } = require('../helpers/db-helpers.js');
 
-User.get = User.findAll();
+//  Use this function to populate the restaurant sub-categories.
+//  This only needs to be done on database init
+const populateCategories = async () => {
+  const categoryOptions = {
+    name: 'Restaurants',
+    alias: 'restaurants',
+  };
+  const { id: categoryId } = await Category.findOrCreate({ where: categoryOptions });
+  const interests = await restCategories();
+  interests.forEach((interest) => {
+    const interestOptions = {
+      name: interest.title,
+      alias: interest.alias,
+      parentId: categoryId,
+    };
+    Category.findOrCreate({ where: interestOptions });
+  });
+};
+
+//  Feel free to comment this call out after the first run
+populateCategories();
 
 module.exports = {
   User,
