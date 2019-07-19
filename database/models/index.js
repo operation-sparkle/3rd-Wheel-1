07a0e6e28a1754eea9851ec024887bcc18a4d064
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 14:31:20 -0500

    Use try / catch in populateCategories

--- database/models/index.js
+++ database/models/index.js
@@ -6,20 +6,24 @@ const { restCategories } = require('../helpers/db-helpers.js');
 //  Use this function to populate the restaurant sub-categories.
 //  This only needs to be done on database init
 const populateCategories = async () => {
-  const categoryOptions = {
-    name: 'Restaurants',
-    alias: 'restaurants',
-  };
-  const { id: categoryId } = await Category.findOrCreate({ where: categoryOptions });
-  const interests = await restCategories();
-  interests.forEach((interest) => {
-    const interestOptions = {
-      name: interest.title,
-      alias: interest.alias,
-      parentId: categoryId,
+  try {
+    const categoryOptions = {
+      name: 'Restaurants',
+      alias: 'restaurants',
     };
-    Category.findOrCreate({ where: interestOptions });
-  });
+    const { id: categoryId } = await Category.findOrCreate({ where: categoryOptions });
+    const interests = await restCategories();
+    interests.forEach((interest) => {
+      const interestOptions = {
+        name: interest.title,
+        alias: interest.alias,
+        parentId: categoryId,
+      };
+      Category.findOrCreate({ where: interestOptions });
+    });
+  } catch (err) {
+    console.error(`Populate Categories failed: ${err}`);
+  }
 };
 
 //  Feel free to comment this call out after the first run
