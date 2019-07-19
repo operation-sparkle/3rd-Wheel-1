aef9c4fa094a30232146e1d3cc242b074c746d7e
Operation Spark <miguelpeter2@gmail.com>
2019-07-19 17:46:39 -0500

    Set a template carousel on the page. Needs to be updated for whitetext

--- client/src/components/Matches.jsx
+++ client/src/components/Matches.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image'
+import Carousel from 'react-bootstrap/Carousel'
 
 
-const Matches = () => {
+const Matches = (obj) => {
   return (
-    <div>My Matches</div>
+    <div>My Matches
+    <Carousel>
+      <Carousel.Item>
+        <img
+        className={obj.name}
+        src="https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg"
+        height="800px"
+        width="750px"
+        alt="No Image Given"
+        />
+        <Carousel.Caption>
+          <h3>Name</h3>
+          <p>Hook</p>
+          <p>Interest</p>
+          <p>Interest</p>
+          <p>Interest</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
   );
 }
 
