3b55cbc9e5e878b21a1e0d85c889b6b3d70651e8
Operation Spark <miguelpeter2@gmail.com>
2019-07-20 16:24:19 -0500

    Refactored Carousel to be dynamic with users. Added Skip button

--- client/src/components/Matches.jsx
+++ client/src/components/Matches.jsx
@@ -1,37 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Image from 'react-bootstrap/Image';
 import Carousel from 'react-bootstrap/Carousel';
+import CarouselItem from 'react-bootstrap/CarouselItem'
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import CardImg from 'react-bootstrap/CardImg';
+import Axios from 'axios';
 
 
 
-const Matches = () => {
+const Matches = (props) => {
+  const [index, changeIndex] = useState(0);
 
+
+  function handleChange() {
+    changeIndex(index++);
+    setDirection(index++);
+  }
+
+
+  function newMatch(users) {
+   return users.map((user) => {
+      return (
+        <CarouselItem>
+          <Card>
+            <CardImg width="70%" height="60%" src={ user.image || "https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg" } />
+            <Card.Title>{user.name || "Error"}</Card.Title>
+            <Card.Text>
+              <div>Age: {user.age || "Error"}</div>
+              <div>Interest: {`${user.interest1, user.interest2, user.interest3}` || "An error has ocurred."}</div>
+            </Card.Text>
+          </Card>
+        </CarouselItem>
+      )
+    })
+  }
+  function getNewMatch() {
+    Axios.get(`/matches/userId=${props.userId}`)
+    .then((user) => {
+      console.log(user);
+    }).catch((err) => {
+      console.error(`There was an error: ${err}`);
+    })
+  }
 
 
   return (
-    <Carousel>
-      <Carousel.Item>
-        <Card>
-          <Card.Img src="https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg" />
-          <Card.Body>
-            <Card.Title>Name</Card.Title>
+    <div>
+    <Carousel controls={false} activeIndex={index} dir="left">
+        <CarouselItem>
+          <Card>
+            <CardImg width="70%" height="60%" src={"https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg"} />
+            <Card.Title>{"Find a Match"}</Card.Title>
             <Card.Text>
-              <div>Age</div>
-              <div>Interest1</div>
-              <div>Interest2</div>
-              <div>Interest3</div>
+              <div>Age: {"???"}</div>
+              <div>Interest: {"???"}</div>
             </Card.Text>
-          </Card.Body>
-            <Button variant="success" size="lg" block>Accept</Button>
-            <Button variant="danger" size="lg" block>Decline</Button>
-        
-        </Card>
-      </Carousel.Item>
+          </Card>
+        </CarouselItem>
+      {}
     </Carousel>
+      <Button variant="success" size="lg" block>Accept</Button>
+      <Button onClick={getNewMatch} variant="warning" size="lg" block>Skip</Button>
+      <Button variant="danger" size="lg" block>Reject</Button>
+    </div>
 
   );
 }
