4a963f8b97cf56b7283ea1d5edb341b08ce4c763
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 19:55:28 -0500

    Implement haversineDistance for manipulating coord

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -15,6 +15,30 @@ const getRestaurant = (location, categories) => {
     .catch(err => console.log(err, 'axios error'));
 };
 
+const haversineDistance = (coords1, coords2) => {
+  const toRadians = (x) => {
+    return x * Math.PI / 180;
+  };
+
+  const [lon1, lat1] = coords1;
+  const [lon2, lat2] = coords2;
+
+  const R = 6371;
+
+  const x1 = lat2 - lat1;
+  const x2 = lon2 - lon1;
+  const dLat = toRadians(x1);
+  const dLon = toRadians(x2);
+
+  const a = Math.sin(dlat / 2) * Math.sin(dlat / 2) +
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * 
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+  return distance;
+};
+
 module.exports = {
   getRestaurant,
+  haversineDistance,
 };
