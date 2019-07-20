f85ccab966a3a834b5f9f821524e20217df95d6f
AS Wilson <alexander.wilson@ncf.edu>
2019-07-20 12:13:26 -0500

    Implement a fetchRestaurant helper

--- database/helpers/db-helpers.js
+++ database/helpers/db-helpers.js
@@ -22,6 +22,23 @@ const restCategories = () => {
     });
 };
 
+const fetchRestraurant = async (category, latitude, longitude) {
+  const { alias } = category;
+  const options = {
+    method: 'get',
+    url: 'https://api.yelp.com/v3/businesses/search',
+    headers: {
+      'Authorization': `Bearer ${process.env.YELP_KEY}` 
+      latitude,
+      longitude,
+      alias,
+    }
+  }
+  const response = await axios(options);
+  const { businesses } = response.data;
+  return businesses[0];
+}
+
 //  This function will find the distance between two coordinates
 //  It expects arrays of [longitude, latitude]
 const haversineDistance = (coords1, coords2) => {
@@ -64,6 +81,7 @@ const topInterest = (interests) => {
 
 module.exports = {
   restCategories,
+  fetchRestraurant,
   haversineDistance,
   topInterest,
 };
