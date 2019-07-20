3ffe5ab7299114fd9c5c3080ce50f1b2b0108bde
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 21:08:30 -0500

    Fix typo with dLat in haversine

--- database/helpers/db-helpers.js
+++ database/helpers/db-helpers.js
@@ -25,9 +25,7 @@ const restCategories = () => {
 //  This function will find the distance between two coordinates
 //  It expects arrays of [longitude, latitude]
 const haversineDistance = (coords1, coords2) => {
-  const toRadians = (x) => {
-    return x * Math.PI / 180;
-  };
+  const toRadians = x => x * Math.PI / 180;
 
   const [lon1, lat1] = coords1;
   const [lon2, lat2] = coords2;
@@ -39,9 +37,9 @@ const haversineDistance = (coords1, coords2) => {
   const dLat = toRadians(x1);
   const dLon = toRadians(x2);
 
-  const a = Math.sin(dlat / 2) * Math.sin(dlat / 2) +
-    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * 
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+    + Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2))
+    * Math.sin(dLon / 2) * Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const distance = R * c;
   return distance;
