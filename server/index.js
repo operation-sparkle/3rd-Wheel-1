e398e0d54fc176e8d9e069cf6684981b503e9a2d
AS Wilson <alexander.wilson@ncf.edu>
2019-07-20 13:03:42 -0500

    Properly reference user ids in couple

--- server/index.js
+++ server/index.js
@@ -179,8 +179,8 @@ app.post('/matches/:userId', async (req, res) => {
     const matches = await user.findInterests(interests, user);
     const matchId = selectMatch(matches);
     const coupleValues = {
-      user1Id: userId,
-      user2Id: matchId,
+      user1IdId: userId,
+      user2IdId: matchId,
       status: 'pending',
     };
     const couple = await Couple.create(coupleValues);
