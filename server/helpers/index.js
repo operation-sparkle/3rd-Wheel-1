2b6173fa9cfc5deee9165ffdb7287794d2068fc8
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 21:30:23 -0500

    Remove haversineDistance from index helper

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const config = require('../../api-config');
 
-
 const getRestaurant = (location, categories) => {
   const options = {
     url: `https://api.yelp.com/v3/businesses/search?location=${location}&categories=${categories}`,
@@ -15,32 +14,6 @@ const getRestaurant = (location, categories) => {
     .catch(err => console.log(err, 'axios error'));
 };
 
-//  This function will find the distance between two coordinates
-//  It expects arrays of [longitude, latitude]
-const haversineDistance = (coords1, coords2) => {
-  const toRadians = (x) => {
-    return x * Math.PI / 180;
-  };
-
-  const [lon1, lat1] = coords1;
-  const [lon2, lat2] = coords2;
-
-  const R = 6371;
-
-  const x1 = lat2 - lat1;
-  const x2 = lon2 - lon1;
-  const dLat = toRadians(x1);
-  const dLon = toRadians(x2);
-
-  const a = Math.sin(dlat / 2) * Math.sin(dlat / 2) +
-    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * 
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c;
-  return distance;
-};
-
 module.exports = {
   getRestaurant,
-  haversineDistance,
 };
