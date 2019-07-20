40c3db3f254c943fe6e9645ba3b4776a42ed8685
AS Wilson <alexander.wilson@ncf.edu>
2019-07-19 21:04:24 -0500

    Move haversineDistance to db-helpers

--- database/helpers/db-helpers.js
+++ database/helpers/db-helpers.js
@@ -22,6 +22,32 @@ const restCategories = () => {
     });
 };
 
+//  This function will find the distance between two coordinates
+//  It expects arrays of [longitude, latitude]
+const haversineDistance = (coords1, coords2) => {
+  const toRadians = (x) => {
+    return x * Math.PI / 180;
+  };
+
+  const [lon1, lat1] = coords1;
+  const [lon2, lat2] = coords2;
+
+  const R = 6371;
+
+  const x1 = lat2 - lat1;
+  const x2 = lon2 - lon1;
+  const dLat = toRadians(x1);
+  const dLon = toRadians(x2);
+
+  const a = Math.sin(dlat / 2) * Math.sin(dlat / 2) +
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * 
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+  return distance;
+};
+
 module.exports = {
   restCategories,
+  haversineDistance,
 };
