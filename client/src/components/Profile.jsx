433ba59e807582299486bc71ba12f0f354edcfb2
Operation Spark <miguelpeter2@gmail.com>
2019-07-21 18:15:58 -0500

    Profile can now access props and dynamically change on user info

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,31 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 
 const Profile = (props) => {
+  const [user, changeUser] = useState(props.user)
 
   return (
     <div>
     <Card style={{ width: '24rem' }}>
         <Card.Img variant="top" src="https://previews.123rf.com/images/panyamail/panyamail1809/panyamail180900343/109879063-user-avatar-icon-sign-profile-symbol.jpg" />
       <Card.Body>
-        <Card.Title>My Name</Card.Title>
+        <Card.Title>{user.name || "My Name"}</Card.Title>
       </Card.Body>
         <ListGroup className="list-group-first">
-          <ListGroupItem>My Age</ListGroupItem>
-          <ListGroupItem>My Gender</ListGroupItem>
-          <ListGroupItem>My Preference</ListGroupItem>
+          <ListGroupItem>{user.age || "My Age"}</ListGroupItem>
+          <ListGroupItem>{user.gender || "My Gender"}</ListGroupItem>
+          <ListGroupItem>{user.gender || "My Preference"}</ListGroupItem>
         </ListGroup>
       <Card.Body>
         <Card.Text>
-          Here's a small summary of myself.
+          {user.bio || "Here's a small summary of myself."}
         </Card.Text>
       </Card.Body>
       </Card>
