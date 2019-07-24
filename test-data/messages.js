dcc503ee671fa8f3b05e841d14e9fcdd45f6d51e
franco3445 <francoappss@gmail.com>
2019-07-24 16:21:11 -0500

    Adding messages table

---
+++ test-data/messages.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    userId: 1,
+    message: 1,
+    sentFrom: 'BennyBrosh',
+  },
+  {
+    userId: 1,
+    message: 2,
+    sentFrom: 'SessaSessaSessa',
+  },
+  {
+    userId: 1,
+    message: 3,
+    sentFrom: 'jone388',
+  },
+];
